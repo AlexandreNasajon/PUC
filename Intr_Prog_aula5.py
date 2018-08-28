4b57f7c54b5f8a910dc813e4ec565cc8d688dfef
nasajon-chan <alexandrenasajon66@hotmail.com>
2018-08-28 16:17:47 -0300

    Create Intr_Prog_aula5.py

---
+++ Intr_Prog_aula5.py
@@ -0,0 +1,37 @@
+import turtle
+
+frank = turtle.Turtle()
+
+def draw_line(turtle):
+    frank.color('red')
+    frank.fd(100)
+    frank.color('blue')
+    frank.fd(100)
+    return
+
+frank.reset()
+frank.width(10)
+frank.up()
+frank.goto(-300,0)
+frank.down()
+draw_line(frank)
+draw_line(frank)
+draw_line(frank)
+
+frank.reset()
+
+def draw_square(turtle):
+    frank.fd(100)
+    frank.left(90)
+    return
+
+frank.right(45)
+frank.fillcolor('blue')
+frank.begin_fill()
+draw_square(frank)
+draw_square(frank)
+draw_square(frank)
+draw_square(frank)
+frank.end_fill()  
+frank.left(45)
+
