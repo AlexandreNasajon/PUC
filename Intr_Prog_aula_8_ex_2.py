591fbc4f51f0c2a3fb28417a5beb4048133a493d
nasajon-chan <alexandrenasajon66@hotmail.com>
2018-09-06 16:59:12 -0300

    Create Intr_Prog_aula_8_ex_2.py

---
+++ Intr_Prog_aula_8_ex_2.py
@@ -0,0 +1,7 @@
+print( 'Insira uma quantidade de horas:' )
+
+horas = int( input( ) )
+
+distancia_km = horas * 3600
+
+print( 'Nessa quantidade de horas, a Terra terá percorrido ' + str( 30*distancia_km ) + ' km.' )
