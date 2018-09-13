f5136839914b37d625701f8c54fc833c8667aac0
nasajon-chan <alexandrenasajon66@hotmail.com>
2018-09-13 16:46:09 -0300

    Create Intr_Prog_aula9_ex2.py

---
+++ Intr_Prog_aula9_ex2.py
@@ -0,0 +1,42 @@
+import math
+
+altura = int(input("Altura?"))
+raio = int(input("Raio?"))
+
+#qty_latas
+
+#custo_total
+
+#custo_lata = 20 reais
+
+#tinta_lata = 5 litros
+
+#litro_tinta = 3 metros quadrados
+
+comprimento = 2 * math.pi * raio
+
+def area_retangulo( altura , comprimento ):
+    return altura * comprimento    
+
+def area_circulo( raio ):
+    return math.pi * (raio ** 2)
+
+def area_cilindro( raio , altura ):
+    a = area_circulo(raio)
+    b = area_retangulo( altura , comprimento)
+    return a + b
+    
+def qty_latas( raio , altura ):
+    area = area_cilindro( raio , altura )
+    litros_de_tinta = area / 3
+    latas_de_tinta = litros_de_tinta / 5
+    custo_da_lata = 20 * latas_de_tinta
+    return custo_da_lata
+
+print(area_retangulo( altura , comprimento))
+
+print(area_circulo ( raio ))
+
+print(area_cilindro( raio , altura ))
+
+print(qty_latas( raio , altura ))
